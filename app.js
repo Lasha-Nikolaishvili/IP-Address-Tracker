ebbd910ce8c684f497af5d29028efaa44738a3b5
IBSU_Student <IBSU_Student@ibsu.edu.ge>
2023-05-10 16:01:39 +0400

    adding ipify

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+async function setLocation() {
+    try {
+        const response = await axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_1eQQd8woQ4THoGQtqXDVXITIAlZri&ipAddress=8.8.8.8');
+        console.log(response.data.location);
+        const map = L.map('map').setView([response.data.location.lat, response.data.location.lng], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+setLocation();
+// https://geo.ipify.org/api/v2/country,city?apiKey=at_1eQQd8woQ4THoGQtqXDVXITIAlZri&ipAddress=8.8.8.8
